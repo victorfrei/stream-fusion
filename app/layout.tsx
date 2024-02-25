3c50d61fb653d4e91176c3b249b33bbe8eca0031
victorfrei <vic.frei2@gmail.com>
2024-02-25 13:03:55 -0300

    Update: Speed Analytics Watching V1.2.0

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Inter, Fredoka } from "next/font/google";
 import "./globals.css";
 import "plyr-react/plyr.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -27,6 +28,7 @@ export default function RootLayout({
     <html lang="pt-BR" className={inter.className}>
       <body className="bg-background text-text scrollbar-thin scrollbar-corner-transparent scrollbar-thumb-secondary100 scrollbar-track-secondary scrollbar-thumb-rounded-sm">
         <Analytics />
+        <SpeedInsights />
         <main className="min-h-screen flex flex-col items-center">
           {children}
         </main>
