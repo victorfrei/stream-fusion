c5dcf5aa43f0a63056d4380a8d458ae7c1c4e052
victorfrei <vic.frei2@gmail.com>
2024-02-25 12:59:39 -0300

    Update: Analytics Watching V1.1.9

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Inter, Fredoka } from "next/font/google";
 import "./globals.css";
 import "plyr-react/plyr.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -25,6 +26,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" className={inter.className}>
       <body className="bg-background text-text scrollbar-thin scrollbar-corner-transparent scrollbar-thumb-secondary100 scrollbar-track-secondary scrollbar-thumb-rounded-sm">
+        <Analytics />
         <main className="min-h-screen flex flex-col items-center">
           {children}
         </main>
